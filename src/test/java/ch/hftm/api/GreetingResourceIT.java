01b248b30733bfb0795bda76230863b0eb6f66af
jan.buettiker <jan.buettiker@hftm.ch>
2022-06-06 11:08:31 +0200

    Initial setup

---
+++ src/test/java/ch/hftm/api/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package ch.hftm.api;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
