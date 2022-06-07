d9bdb58ba68ee8723f8d24eec1782f86ee6bd691
jan.buettiker <jan.buettiker@hftm.ch>
2022-06-07 18:30:09 +0200

    added simple state query to get and patch states

--- src/main/java/ch/hftm/api/resources/WindowResource.java
+++ src/main/java/ch/hftm/api/resources/WindowResource.java
@@ -6,15 +6,18 @@ import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PATCH;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 
 import ch.hftm.api.models.Window;
+import ch.hftm.api.models.enums.StateType;
 
 @Path("/windows")
 public class WindowResource {
@@ -32,12 +35,41 @@ public class WindowResource {
         return Window.findWindowById(id);
     }
 
+    @GET
+    @Path("/{id}/state")
+    @Produces(MediaType.APPLICATION_FORM_URLENCODED)
+    public StateType getWindowDesiredState(@QueryParam("type") String state, @PathParam("id") Long id) {
+        if (state.equals("desired")) {
+            return Window.findWindowById(id).desiredState;
+        } else if (state.equals("current")) {
+            return Window.findWindowById(id).currentState;
+        } else {
+            throw new WebApplicationException("Unknown state type " + state + "does not exist.", 404);
+        }
+    }
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Transactional
     public Window addWindow(Window window) {
         window.persist();
+        return window;
+    }
+
+    @PATCH
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Window patchWindowState(@PathParam("id") Long id, @QueryParam("type") String type,
+            @QueryParam("value") StateType value) {
+        Window window = Window.findWindowById(id);
+        if (type.equals("current")) {
+            window.currentState = value;
+        } else if (type.equals("desired")) {
+            window.desiredState = value;
+        }
+
         return window;
     }
 
