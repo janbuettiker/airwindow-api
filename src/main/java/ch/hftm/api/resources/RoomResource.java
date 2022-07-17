31beed8c00ec3d3692549b29c9afcab79c2c3639
jan.buettiker <jan.buettiker@hftm.ch>
2022-07-17 15:23:10 +0200

    refactored room resource

--- src/main/java/ch/hftm/api/resources/RoomResource.java
+++ src/main/java/ch/hftm/api/resources/RoomResource.java
@@ -3,6 +3,7 @@ package ch.hftm.api.resources;
 import java.util.List;
 
 import javax.transaction.Transactional;
+import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -11,63 +12,91 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 
 import ch.hftm.api.models.Home;
 import ch.hftm.api.models.Room;
 
-@Path("/rooms")
+@Path("homes/{homeId}/rooms")
 public class RoomResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Room> getRooms() {
-        return Room.findAllRooms();
+    public List<Room> getRooms(@PathParam("homeId") Long homeID) {
+        return Home.findHomeById(homeID).roomList;
     }
 
     @GET
-    @Path("/{id}")
+    @Path("/{roomId}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Room getRoom(@PathParam("id") Long id) {
-        return Room.findRoomById(id);
+    public Room getRoom(
+            @PathParam("homeId") Long homeId,
+            @PathParam("roomId") Long roomId) {
+
+        Home h = Home.findHomeById(homeId);
+        Room r = Room.findRoomById(roomId);
+
+        if (h.roomList.contains(r)) {
+            return r;
+        } else {
+            throw new WebApplicationException("Room with the id: " + r.id + " does not exist in this home.", 404);
+        }
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Transactional
-    public Room addRoom(Room room, @QueryParam("home-id") Long homeId) {
-        Home home = Home.findHomeById(homeId);
-        home.addRoom(room);
+    public Room addRoom(
+            @PathParam("homeId") Long homeId,
+            @Valid Room room) {
+
+        Home h = Home.findHomeById(homeId);
+        h.addRoom(room);
         return room;
     }
 
     @PUT
-    @Path("/{id}")
+    @Path("/{roomId}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Transactional
-    public Room updateRoom(@PathParam("id") Long id, Room room) {
-        Room entity = Room.findRoomById(id);
-        if (entity == null) {
-            throw new WebApplicationException("Entry with id of " + id + " does not exist.", 404);
-        }
-        entity.description = room.description;
-        entity.name = room.name;
-        entity.windowList = room.windowList;
+    public Room updateRoom(
+            @PathParam("homeId") Long homeId,
+            @PathParam("roomId") Long roomId,
+            @Valid Room room) {
 
-        return entity;
+        Home h = Home.findHomeById(homeId);
+        Room r = Room.findRoomById(roomId);
+
+        if (h.roomList.contains(r)) {
+            r.name = room.name;
+            r.description = room.description;
+            r.windowList = room.windowList;
+            return r;
+        } else {
+            throw new WebApplicationException("Room with id: " + r.id + " does not exist in this room", 404);
+        }
     }
 
     @DELETE
-    @Path("/{id}")
+    @Path("/{roomId}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Transactional
-    public boolean deleteRoom(@PathParam("id") Long id) {
-        return Room.deleteRoomById(id);
+    public boolean deleteRoom(
+            @PathParam("homeId") Long homeId,
+            @PathParam("roomId") Long roomId) {
+
+        Home h = Home.findHomeById(homeId);
+        Room r = Room.findRoomById(roomId);
+
+        if (h.roomList.contains(r)) {
+            return h.roomList.remove(r);
+        } else {
+            throw new WebApplicationException("Room with id: " + r.id + " does not exist in this room", 404);
+        }
     }
 
 }
